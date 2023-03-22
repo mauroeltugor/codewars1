b9b4b6aa8c83da866863844ce42f1ca8747d3605
mauro <andrekxus299@gmail.com>
2023-03-21 19:09:04 -0500

    adition with for and for each in a new document

---
+++ arraysAdition.js
@@ -0,0 +1,20 @@
+const nums = [1, 2, 3, 4, 5];
+const sum = (a, b) => a + b;
+const sumNums = [];
+
+
+nums.forEach((_,i) =>{
+  if(i < nums.length-1){
+    const result = sum(nums[i], nums[i+1])
+    sumNums.push(result)
+  }
+})
+
+console.log(sumNums);
+
+for (i = 0; i < nums.length-1; i += 1){
+  let result = sum(nums[i],nums[i + 1] );
+  sumNums.push(result)
+}
+
+console.log(sumNums);
--- explode3.js
+++ explode3.js
@@ -1,20 +1,20 @@
-function explode(x) {
-    let score = 0;
-    let result = [];
+// function explode(x) {
+//     let score = 0;
+//     let result = [];
     
-    if (typeof x[0] !== "number" && typeof x[1] !== "number") {
-      return "Void!";
-    } else if (typeof x[0] !== "number" && typeof x[1] === "number") {
-      score = x[1];
-    } else if (typeof x[0] === "number" && typeof x[1] !== "number") {
-      score = x[0];
-    } else {
-      score = x[0] + x[1];
-    }
+//     if (typeof x[0] !== "number" && typeof x[1] !== "number") {
+//       return "Void!";
+//     } else if (typeof x[0] !== "number" && typeof x[1] === "number") {
+//       score = x[1];
+//     } else if (typeof x[0] === "number" && typeof x[1] !== "number") {
+//       score = x[0];
+//     } else {
+//       score = x[0] + x[1];
+//     }
     
-    for (let i = 0; i < score; i++) {
-      result.push(x);
-    }
+//     for (let i = 0; i < score; i++) {
+//       result.push(x);
+//     }
     
-    return result;
-}
+//     return result;
+// }
